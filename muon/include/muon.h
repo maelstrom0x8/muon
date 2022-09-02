91c8dd814fdef8e5cc42043eac88fbd3a913a53c
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-09-02 01:18:40 +0100

    Added audio info class to generate audio tag for audio items

--- muon/include/muon.h
+++ muon/include/muon.h
@@ -1,10 +1,24 @@
 #ifndef MUON_H
 #define MUON_H
 
+#include <QFileInfo>
+#include <taglib/fileref.h>
+#include <taglib/taglib.h>
+
 namespace muon {
+class AudioInfo {
+public:
+  AudioInfo *operator()(const QString &filename);
 
+  QString getTitle() const;
+  QString getArtist() const;
+  QString getFileName() const;
 
+private:
+  TagLib::FileRef ref;
+  const char *_filename;
+};
 
-}
+} // namespace muon
 
 #endif
--- muon/src/muon.cc
+++ muon/src/muon.cc
@@ -1,2 +1,22 @@
 #include "muon.h"
 
+muon::AudioInfo *muon::AudioInfo::operator()(const QString &filename) {
+  auto str = filename.toLocal8Bit().data();
+  _filename = TagLib::FileName(str);
+  ref = TagLib::FileRef(_filename);
+
+  return this;
+}
+
+QString muon::AudioInfo::getTitle() const {
+  if (QString(ref.tag()->title().toCString()) == "")
+    return QFileInfo(_filename).fileName();
+  else
+    return QString(ref.tag()->title().toCString());
+}
+
+QString muon::AudioInfo::getArtist() const {
+  return QString(ref.tag()->artist().toCString()).toUtf8();
+}
+
+QString muon::AudioInfo::getFileName() const { return QString(_filename); }
